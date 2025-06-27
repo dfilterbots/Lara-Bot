f128b3769396d48ff66d08dd47b570a12a6a3b61
Jazin <ddfilterbot01@gmail.com>
2025-06-27 08:32:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ then
   echo "Cloning main Repository"
   git clone https://github.com/lazyindu/test1.git /LazyPrincess
 else
+fi
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /LazyPrincess
 fi
